c63c04790938d2a20f00ed2a0907974234ddf5b1
Prashant <prashant@mac.com>
2017-01-28 20:36:16 -0600

    Week 5 Assignment Updates

--- module5-solution/src/public/registration/registration.controller.js
+++ module5-solution/src/public/registration/registration.controller.js
@@ -1,31 +1,39 @@
-(function () {
-"use strict";
+(function() {
+    "use strict";
 
-angular.module('public')
-.controller('RegistrationController', RegistrationController);
+    angular.module('public')
+        .controller('RegistrationController', RegistrationController);
 
-RegistrationController.$inject = ['MenuService'];
-function RegistrationController(MenuService) {
-  var reg = this;
-  reg.response = '';
+    RegistrationController.$inject = ['MenuService'];
 
+    function RegistrationController(MenuService) {
+        var reg = this;
+        reg.response = '';
+        reg.Message = '';
+        reg.favoriteFound = false;
 
-  reg.submit = function () {
-  MenuService.getItem(reg.user.favorite.toUpperCase()).then(function(response) {
-      if (response === false) {
-        reg.response = 'No Such menu item exists';
-      }
-      else {
-          reg.response = "Your information has been saved";
-          reg.user.favorite = reg.user.favorite.toUpperCase();
-          reg.user.name = response.name;
-          reg.user.description = response.description;
-          reg.user.response = "Your information has been saved";
-          MenuService.setuser(reg.user);
-      }
-    })
-  };
+        reg.callService = function() {
+            MenuService.getItem(reg.user.favorite.toUpperCase()).then(function(response) {
+                if (response === false) {
+                    reg.Message = 'No Such menu item exists';
+                } else {
+                    reg.favoriteFound = true;
+                    reg.Message = 'Menu Item Found';
+                    reg.user.favorite = reg.user.favorite.toUpperCase();
+                    reg.user.name = response.name;
+                    reg.user.description = response.description;
 
-}
+                }
+            })
+
+        };
+
+        reg.submit = function() {
+            reg.response = "Your information has been saved";
+            reg.user.response = "Your information has been saved";
+            MenuService.setuser(reg.user);
+        };
+
+    }
 
 })();
